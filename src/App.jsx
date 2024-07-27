58e3ab54d08b4a9970887e4de4c26f0218112f6c
osuchlorena <osuchlorena@gmail.com>
2024-07-27 11:37:57 -0400

    1.12*: anecdotes step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { useState } from 'react'
-import Button from './components/Button'
-import Statistics from './components/Statistics'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  function handleClickGood(){
-    setGood(good + 1)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  function getRandomInt() {
+    return Math.floor(Math.random() * 7);
   }
-  function handleClickNeutral(){
-    setNeutral(neutral + 1)
-  }
-  function handleClickBad(){
-    setBad(bad + 1)
+  function handleClick(){
+    setSelected(getRandomInt)
   }
   return (
-    <>
-      <h1>Give Fidebaack</h1>
-      <Button onClickButton={handleClickGood} text="good"/>
-      <Button onClickButton={handleClickNeutral} text="neutral"/>
-      <Button onClickButton={handleClickBad} text="bad"/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-    </>
+    <div style={{display: 'flex', flexDirection:"column"}}>
+      {anecdotes[selected]}
+      <button onClick={handleClick} style={{width:"100px"}} > next anecdote
+      </button>
+    </div>
   )
 }
 
